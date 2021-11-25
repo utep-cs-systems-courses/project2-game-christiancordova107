c18d64236f864448a59a2a44a5c2b34c1052d425
christiancordova107 <cjcordova@miners.utep.edu>
2021-11-25 15:01:41 -0700

    added logic for jump table

--- project/Dim25Green-s.s
+++ project/Dim25Green-s.s
@@ -1,22 +1,25 @@
+	.text
+JT:	.word Default
+	.word case1
+	.word case2
+	.word case3
+	.word case4
+
 	.arch msp430g2553
 	.p2align 1,0
-	.text
 	
 	.extern greenDim
 	.extern Green_On_Off_Flag
 	.global Dim25Green
 Dim25Green:
-	cmp #1, &greenDim
-	jz case1
+	mov &greenDim, R12
 
-	cmp #2, &greenDim
-	jz case2
+	cmp #5, R12
+	jhs Default		;check if greenDim > 4
+	
 
-	cmp #3, &greenDim
-	jz case3
-
-	cmp #4, &greenDim
-	jz case4
+	add R12, R12
+	mov JT(R12), R0
 
 case1:
 	mov #1, &Green_On_Off_Flag
@@ -37,5 +40,8 @@ case4:
 	mov #0, &Green_On_Off_Flag
 	mov #1, &greenDim
 	jmp Exit
+
+Default: jmp Exit
+	
 
 Exit:	pop r0
